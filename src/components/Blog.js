7333a0f2a25f67ade88323c56e65cbec7e07c1ea
sebski123 <sebthomle@gmail.com>
2022-02-18 20:36:39 +0100

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: "solid",
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+const Blog = ({ blog }) => {
+  const [showAllInfo, setShowAllInfo] = useState(false);
+
+  const toggleShowAllInfo = () => setShowAllInfo(!showAllInfo);
+
+  return (
+    <div style={blogStyle}>
+      <div style={{ display: showAllInfo ? "none" : "" }}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleShowAllInfo}>view</button>
+      </div>
+      <div style={{ display: showAllInfo ? "" : "none" }}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                {blog.title}{" "}
+                <button onClick={toggleShowAllInfo}>hide</button>
+              </td>
+            </tr>
+            <tr>
+              <td>{blog.url}</td>
+            </tr>
+            <tr>
+              <td>
+                {blog.likes} <button>like</button>
+              </td>
+            </tr>
+            <tr>
+              <td>{blog.author}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
