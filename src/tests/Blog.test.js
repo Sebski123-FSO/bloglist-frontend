39fcdd86c4634bfa5d923505af50ecb12ac96e04
sebski123 <sebthomle@gmail.com>
2022-02-20 16:39:01 +0100

    5.14*: Blog list tests, step2

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Blog from "../components/Blog";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 describe("<Blog />", () => {
   const testBlog = {
@@ -37,5 +37,18 @@ describe("<Blog />", () => {
     expect(basicInfo).toBeVisible();
     expect(url).not.toBeVisible();
     expect(likes).not.toBeVisible();
+  });
+
+  test("More content rendered after button press", () => {
+    const button = screen.getByText("view");
+    userEvent.click(button);
+
+    const basicInfo = screen.getByTestId("basicInfo");
+    const url = screen.queryByText("http://blog.google.com");
+    const likes = screen.queryByTestId("likes");
+
+    expect(basicInfo).not.toBeVisible();
+    expect(url).toBeVisible();
+    expect(likes).toBeVisible();
   });
 });
